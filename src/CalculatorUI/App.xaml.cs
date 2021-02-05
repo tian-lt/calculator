42c5378c30427f0624cdf98a0fe9ac09c8390436
MSFT-Tilia <tilia@microsoft.com>
2021-02-05 11:01:27 +0800

    App.xaml.cs: removes the deriving statement for class App

--- src/CalculatorUI/App.xaml.cs
+++ src/CalculatorUI/App.xaml.cs
@@ -54,7 +54,7 @@ namespace CalculatorApp
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App
     {
         /// <summary>
         /// Initializes the singleton application object. This is the first line of authored code
