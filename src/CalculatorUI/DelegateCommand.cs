c5b99685c40910a5a960379099f7a2684f65d8dd
MSFT-Tilia <tilia@microsoft.com>
2021-02-07 15:38:59 +0800

    add CalculatorProgrammerDisplayPanel.xaml

---
+++ src/CalculatorUI/DelegateCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace CalculatorApp.Common
+{
+    internal class DelegateCommand<TTarget> : System.Windows.Input.ICommand
+    {
+        public delegate void CommandHandlerFunc(object obj);
+
+        public DelegateCommand(TTarget target, CommandHandlerFunc func)
+        {
+            m_weakTarget = new WeakReference(target);
+            m_function = func;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            object target = m_weakTarget.Target;
+            if(target != null && target is TTarget)
+            {
+                m_function(parameter);
+            }
+        }
+
+        private CommandHandlerFunc m_function;
+        private WeakReference m_weakTarget;
+    }
+}
