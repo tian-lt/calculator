1c81a74ba674a58ccb90e9548bce123cadfd84a5
MSFT-Tilia <tilia@microsoft.com>
2021-02-25 18:24:47 +0800

    add Controls/RadixButton.cs

---
+++ src/CalculatorUI/Controls/RadixButton.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using CalculatorApp;
+using CalculatorApp.Common;
+using CalculatorApp.Controls;
+
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Data;
+
+namespace CalculatorApp
+{
+    namespace Controls
+    {
+        public sealed class RadixButton : Windows.UI.Xaml.Controls.RadioButton
+        {
+            public RadixButton()
+            {}
+
+            internal string GetRawDisplayValue()
+            {
+                string radixContent = Content.ToString();
+                return LocalizationSettings.GetInstance().RemoveGroupSeparators(radixContent);
+            }
+        }
+    }
+}
