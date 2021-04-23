0c7e3e1095c963b1cee459d770be15108a901587
MSFT-Tilia <tilia@microsoft.com>
2021-04-23 18:39:32 +0800

    fixes warning at NavCategory.cpp@72

--- src/CalcViewModel/Common/NavCategory.cpp
+++ src/CalcViewModel/Common/NavCategory.cpp
@@ -65,11 +65,10 @@ bool IsGraphingModeEnabled()
     // CSHARP_MIGRATION: TODO: merge PR ##1471 to fix below bug before we release to PROD
 #ifdef _DEBUG
     _isGraphingModeEnabledCached = true;
+    return _isGraphingModeEnabledCached->Value;
 #else
     throw "CSHARP_MIGRATION: EXCEPTION";
 #endif // _DEBUG
-
-    return _isGraphingModeEnabledCached->Value;
 }
 
 // The order of items in this list determines the order of items in the menu.
